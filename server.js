9093ec0d168a563c168402468c4c93d45d4c6243
James Capps <jcapps@protonmail.com>
2020-03-02 14:52:54 -0600

    Fixed typo

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
   app.use(express.static( 'client/build' ))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.thml'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
